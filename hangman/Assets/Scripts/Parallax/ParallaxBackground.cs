1a78b241bde47de8e4ad106c37a8687ecd72d795
Oliver Thunæs <Felolis30@gmail.com>
2017-11-16 15:44:32 +0100

    Added parallax

---
+++ hangman/Assets/Scripts/Parallax/ParallaxBackground.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Background/Parallax Background")]
+public class ParallaxBackground : ScriptableObject
+{
+    public Vector2 scrollFactor = new Vector2(1, 1);
+
+    public Vector2 offset;
+}
---
+++ hangman/Assets/Scripts/Parallax/ParallaxManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ParallaxManager : MonoBehaviour
+{
+    public Background[] backgrounds;
+
+    private Camera _mainCam;
+
+    private void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        SetBackgroundPosition();
+    }
+
+    private void SetBackgroundPosition()
+    {
+        foreach (Background bg in backgrounds)
+        {
+            Vector2 offset;
+            offset.x = (_mainCam.transform.position.x - transform.position.x) * bg.values.scrollFactor.x / 1000 + bg.values.offset.x;
+            offset.y = 0;
+            //            offset.y = (transform.position.y - (_mainCam.transform.position.y + 288)) * bg.values.scrollFactor.y + bg.values.offset.y;
+
+            bg.transform.position = new Vector2(_mainCam.transform.position.x, _mainCam.transform.position.y);
+
+            bg.GetComponent<Renderer>().material.mainTextureOffset = offset;
+        }
+    }
+}
